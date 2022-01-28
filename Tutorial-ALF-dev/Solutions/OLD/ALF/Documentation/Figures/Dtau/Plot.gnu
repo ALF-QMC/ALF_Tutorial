ed84c4d6708748b7af28598884df534373b30804
Jefferson Stafusa Elias Portela <jstafusa@physik.uni-wuerzburg.de>
2022-01-28 11:53:55 +0100

    Cleaning up.

--- Tutorial-ALF-dev/Solutions/OLD/ALF/Documentation/Figures/Dtau/Plot.gnu
+++
@@ -1,4 +0,0 @@
-  plot "NoSymNoCheck/Ener.dat" u ($1*$1):2:3 w e 
-replot "SymCheck/Ener.dat" u ($1*$1):2:3 w e 
-replot "SymNoCheck/Ener.dat" u ($1*$1):2:3 w e 
-replot "NoSymCheck/Ener.dat" u ($1*$1):2:3 w e 
--- Tutorial-ALF-dev/Solutions/OLD/ALF/Prog/Git_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -d ../.git ]
-then 
-  echo "#define GIT" > git.h
-  echo "#define GIT_COMMIT_HASH \"$(git log -1 --format=%h)\"" >> git.h
-  echo "#define GIT_BRANCH \"$(git rev-parse --abbrev-ref HEAD)\"" >> git.h
-else
-  echo "" > git.h
-fi
