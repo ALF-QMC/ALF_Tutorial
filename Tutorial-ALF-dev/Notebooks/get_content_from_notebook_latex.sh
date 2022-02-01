d0d10bf329f6921f091f023937615fce55223cad
stafusa <jstafusa@physik.uni-wuerzburg.de>
2022-02-01 17:58:27 +0100

    Improve script for importing pyALF notebooks into tutorial, ; import notebooks into Tutorial 2.2.

--- Tutorial-ALF-dev/Notebooks/get_content_from_notebook_latex.sh
+++ Tutorial-ALF-dev/Notebooks/get_content_from_notebook_latex.sh
@@ -8,6 +8,7 @@ newfile="${file%.tex}-content.tex"              # "file" minus ".tex" extension 
 sed -e '/maketitle/,/end{document}/!d'        \
     -e '/maketitle/d;/end{document}/d'        \
     -e "s/{\([^{]*.png}\)/{${file%.tex}-\1/g" \
+    -e 's/adjustimage{max size={0.9/adjustimage{max size={0.6/g' \
     $file  >  $newfile                          # Where: 
                                                 # 1st: delete lines NOT in the range between expressions
                                                 # 2nd: delete lines containing the expressions
@@ -15,3 +16,4 @@ sed -e '/maketitle/,/end{document}/!d'        \
                                                 #      \1 refers to the previous subexpression enclosed by
                                                 #      \( \), and [^{]* matches the longest sequence of
                                                 #      characters not including {
+                                                # 4th: change image max size from 0.9 to 0.6
